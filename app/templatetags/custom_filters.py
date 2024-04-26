55035cac118dfc0a88d123ce2b02f2f5fedd41cc
Mathias Weyland <hb9frv@uska.ch>
2024-04-26 22:35:54 +0200

    Custom filters to count questions within pools and (sub)categories

--- app/templatetags/custom_filters.py
+++ app/templatetags/custom_filters.py
@@ -7,3 +7,11 @@ register = template.Library()
 def in_pool(subcategory, pool):
     categories_in_pool = Question.objects.filter(pool__pool_name=pool).values_list('subcategory', flat=True)
     return subcategory.filter(pk__in=categories_in_pool)
+
+@register.filter
+def category_in_pool_count(category, pool):
+    return Question.objects.filter(pool__pool_name=pool, subcategory__parent__pk=category.pk).count()
+
+@register.filter
+def subcategory_in_pool_count(subcategory, pool):
+    return Question.objects.filter(pool__pool_name=pool, subcategory__pk=subcategory.pk).count()
